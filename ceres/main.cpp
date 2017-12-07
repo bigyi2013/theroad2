bd082cf908dc4c9ded6aa2813bb3c8fcd7419c62
yehoo <sun163sun163@126.com>
2017-12-07 22:13:42 +0800

    解决了eigen问题，想明白了ceres方法。

--- ceres/main.cpp
+++ ceres/main.cpp
@@ -6,7 +6,59 @@
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
+#include "ceres/ceres.h"
+#include "rotation.h"
 using namespace std;
+class SnavelyReprojectionError
+{
+public:
+    SnavelyReprojectionError(double observation_x, double observation_y):observed_x(observation_x),observed_y(observation_y){}
+
+    template<typename T>
+    bool operator()(const T* const camera,
+                    const T* const point,
+                    T* residuals)const{
+        // camera[0,1,2] are the angle-axis rotation.
+        T p[3];
+        AngleAxisRotatePoint(camera, point, p);
+        // camera[3,4,5] are the translation.
+        p[0] += camera[3]; p[1] += camera[4]; p[2] += camera[5];
+
+        // Compute the center of distortion. The sign change comes from
+        // the camera model that Noah Snavely's Bundler assumes, whereby
+        // the camera coordinate system has a negative z axis.
+        T xp = - p[0] / p[2];
+        T yp = - p[1] / p[2];
+
+        // Apply second and fourth order radial distortion.
+        const T& l1 = camera[7];
+        const T& l2 = camera[8];
+        T r2 = xp*xp + yp*yp;
+        T distortion = T(1.0) + r2  * (l1 + l2  * r2);
+
+        // Compute final projected point position.
+        const T& focal = camera[6];
+        T predicted_x = focal * distortion * xp;
+        T predicted_y = focal * distortion * yp;
+
+        // The error is the difference between the predicted and observed position.
+        residuals[0] = predicted_x - T(observed_x);
+        residuals[1] = predicted_y - T(observed_y);
+
+        return true;
+    }
+
+    static ceres::CostFunction* Create(const double observed_x, const double observed_y){
+        return (new ceres::AutoDiffCostFunction<SnavelyReprojectionError,2,9,3>(
+                new SnavelyReprojectionError(observed_x,observed_y)));
+    }
+
+
+private:
+    double observed_x;
+    double observed_y;
+};
+
 int main(){
     cv::VideoCapture inputVideo;
     inputVideo.open(0);
